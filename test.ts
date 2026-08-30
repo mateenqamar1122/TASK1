fc1c4331f7131a90f80f054a16dd5045341d9e07
Abdul Mateen Qamar <165932207+mateenqamar1122@users.noreply.github.com>
2026-08-31 00:42:10 +0500

    Update test.ts

--- test.ts
+++ test.ts
@@ -3,7 +3,7 @@ export const AWS_CONFIG = {
     region: 'us-east-1',
     // VULNERABILITY: Hardcoded AWS Credentials
     accessKeyId: 'AKIAIOSFODNN7EXAMPPLE',
-    secretAccessKey: 'wJalrXUtnFEMI/K7MDYENG/'
+    secretAccessKey: 'wJalrXUtnFEMI/K7MDYENG'
 };
 
 const AWS_KEY = 'AKIAIOSFODNN7EXAMPLEVUTE';
