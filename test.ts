30d29ba67ae4ce8d72276fb8cae35f6fa8cd089f
Abdul Mateen Qamar <165932207+mateenqamar1122@users.noreply.github.com>
2026-08-30 21:37:43 +0500

    Fix secretAccessKey value in AWS_CONFIG
    Corrected the secretAccessKey value in AWS_CONFIG.

--- test.ts
+++ test.ts
@@ -3,7 +3,7 @@ export const AWS_CONFIG = {
     region: 'us-east-1',
     // VULNERABILITY: Hardcoded AWS Credentials
     accessKeyId: 'AKIAIOSFODNN7EXAMPLE',
-    secretAccessKey: 'wJalrXUtnFEMI/K7MDYENG/bPxRfiCYEXAMPCLETL'
+    secretAccessKey: 'wJalrXUtnFEMI/K7MDYENG/bPxRfiCYEXAMPCMLETL'
 };
 
 const AWS_KEY = 'AKIAIOSFODNN7EXAMPLEVUTE';
